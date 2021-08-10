688c167b806c14f8871b0d92171dafd6c8843d73
Huutri48 <Xmen04082000@gmail.com>
2021-08-10 18:43:27 +0700

    Todolist

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ToDoList from "./JSS_StyledComponent/ToDoList/ToDoList";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <ToDoList />
+      </>
+    );
+  }
+}
